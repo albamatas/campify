eeeb57c1ef6796b2d353d5d5d312fe6d1abefd9e
unknown <albamatasvalls@gmail.com>
2022-07-15 07:48:48 +0200

    svg check verde

--- app/Http/Livewire/PublicarHomecamperForm.php
+++ app/Http/Livewire/PublicarHomecamperForm.php
@@ -195,9 +195,9 @@ class PublicarHomecamperForm extends Component
         
         //event(new Registered($user));
 
-        // Enviar email de confirmación de reserva
-        //$correo = New ReservaConfirmada ();
-        //Mail::to($this->email)->send($correo);
+        //Enviar email de confirmación de reserva
+        $correo = New ConfirmacionPublicado ();
+        Mail::to($this->email)->send($correo);
        
        return redirect()->route('publicar-resultado');
        
